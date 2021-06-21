7e8aa10f88a210d23019b0db6dc9a2c3b4c910db
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-06-21 08:26:15 -0300

    Render prototype feed data on the table view

--- Prototype/FeedImageCell.swift
+++ Prototype/FeedImageCell.swift
@@ -11,6 +11,6 @@ final class FeedImageCell: UITableViewCell {
     
     @IBOutlet private(set) var locationContainer: UIStackView!
     @IBOutlet private(set) var locationLabel: UILabel!
-    @IBOutlet private(set) var feedImageView: UIView!
+    @IBOutlet private(set) var feedImageView: UIImageView!
     @IBOutlet private(set) var descriptionLabel: UILabel!
 }
